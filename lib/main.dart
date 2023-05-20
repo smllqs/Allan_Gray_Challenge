5283f35600d9b6b4c6bfbba5c1300386f12989ab
Somelele <qsxsom001@myuct.ac.za>
2023-05-20 14:41:36 +0200

    updated main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
-import 'package:app_load_testing_ag/world_time_view.dart';
 import 'package:flutter/material.dart';
-
+import 'package:app_load_testing_ag/world_time_view.dart';
 import 'choose_location.dart';
-import 'home.dart';
 import 'loading.dart';
 
 void main() => runApp(MaterialApp(
@@ -15,37 +13,3 @@ void main() => runApp(MaterialApp(
   },
 ));
 
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-//
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       title: 'Http Test',
-//       theme: ThemeData(
-//         primarySwatch: Colors.blue,
-//       ),
-//       home: HomePage(),
-//     );
-//   }
-// }
-
-// HomePage()
-// ChooseLocation()
-// WorldTimeView()
-// Loading()
-
-// import 'package:flutter/material.dart';
-// import 'package:world_time/Pages/choose_location.dart';
-// import 'package:world_time/Pages/home.dart';
-// import 'package:world_time/Pages/loading.dart';
-// void main() => runApp(MaterialApp(
-//   initialRoute: '/',
-//   routes: {
-//     '/': (context) => Loading(),
-//     '/home' : (context) => Home(),
-//     '/location': (context) => ChooseLocation()
-//   },
-// ));
